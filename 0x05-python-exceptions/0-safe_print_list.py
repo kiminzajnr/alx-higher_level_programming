ac033b9158e7949d6ed9422fb9aa0437127e1e9d
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-17 19:48:23 +0300

    prints x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     num_of_elem = 0
+    len_list = 0
+    for item in my_list:
+        len_list += 1
     try:
         for elem in my_list:
-            while num_of_elem < x:
+            while num_of_elem < x and num_of_elem < len_list:
                 num_of_elem += 1
             print("{}".format(elem), end="")
         print()
         return num_of_elem
     except:
-        print("out of range")
+        pass
