97962fb86a35ed2cbeb6785ed74a2b57a03db367
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-23 10:53:15 +0300

    added code to handle empty names

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,6 +8,8 @@ def say_my_name(first_name, last_name=""):
     """
         prints my name
     """
+    if first_name == "" and last_name == "":
+        return None
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
