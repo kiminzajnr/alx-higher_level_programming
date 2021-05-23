5406031ab7f927098bb43755bcfde25d36b1aea4
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-23 10:11:01 +0300

    Prints my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    No module allowed to be imported
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+        prints my name
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
