f7502a7bd5fdc59ebe6a211a8be3066ff53c5780
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-26 16:18:02 +0300

    add setters and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,10 +5,27 @@ Defines a rectancle
 
 
 class Rectangle:
+    """rectangle class"""
     def __init__(self, width=0, height=0):
         """Instantiation of width and height"""
         self.__width = width
         self.__height = height
+
+    @property
+    def height(self):
+        """retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set width"""
+        self.__height = value
+        try:
+            assert isinstance(self.__height, int)
+        except:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
 
     @property
     def width(self):
@@ -17,27 +34,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set width"""
+        """set height"""
         self.__width = value
         try:
             assert isinstance(self.__width, int)
-        except:
+        except BaseException:
             raise TypeError("width must be an integer")
         if self.__width < 0:
             raise ValueError("width must be >= 0")
-
-    @property
-    def height(self):
-        """retrieve width"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """set height"""
-        self.__height = value
-        try:
-            assert isinstance(self.__height, int)
-        except BaseException:
-            raise TypeError("width must be an integer")
-        if self.__height < 0:
-            raise ValueError("height must be >= 0")
