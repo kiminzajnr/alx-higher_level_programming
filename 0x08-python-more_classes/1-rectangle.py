c8d926feb84c2d5d4455113e3db45b6d716f0fbd
Erick Kiminza <kiminzaeric@gmail.com>
2021-05-26 15:33:50 +0300

    added property setter and getters to rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,33 +7,37 @@ Defines a rectancle
 class Rectangle:
     def __init__(self, width=0, height=0):
         """Instantiation of width and height"""
-        self.__width = 0
-        self.__height = 0
+        self.__width = width
+        self.__height = height
 
+    @property
     def width(self):
         """retrieve width"""
         return self.__width
 
-    def height(self):
-        """retrieve height"""
-        return self.__height
-
+    @width.setter
     def width(self, value):
         """Set width"""
         self.__width = value
         try:
-            assert isinstance(value, int)
-        except BaseException:
+            assert isinstance(self.__width, int)
+        except:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if self.__width < 0:
             raise ValueError("width must be >= 0")
 
+    @property
+    def height(self):
+        """retrieve width"""
+        return self.__height
+
+    @height.setter
     def height(self, value):
         """set height"""
         self.__height = value
         try:
-            assert isinstance(value, int)
+            assert isinstance(self.__height, int)
         except BaseException:
             raise TypeError("width must be an integer")
-        if self.__height != int:
+        if self.__height < 0:
             raise ValueError("height must be >= 0")
