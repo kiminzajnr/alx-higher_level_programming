690ab6685b610b74b569d96939e950a1e63d7442
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-05 20:37:50 +0300

    added validators for all setter methods and instantiation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,11 +8,46 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
+
         super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        try:
+            assert isinstance(self.__width, int)
+        except:
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
+
+        try:
+            assert isinstance(self.__height, int)
+        except:
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
+
+        try:
+            assert isinstance(self.id, int)
+        except:
+            raise TypeError("id must be an integer")
+        if self.id < 0:
+            raise ValueError("id must be >= 0")
+
+        try:
+            assert isinstance(self.__x, int)
+        except:
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
+
+        try:
+            assert isinstance(self.__y, int)
+        except BaseException:
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
 
     @property
     def width(self):
@@ -23,6 +58,12 @@ class Rectangle(Base):
     def width(self, value):
         """set width"""
         self.__width = value
+        try:
+            assert isinstance(self.__width, int)
+        except:
+            raise TypeError("width must be an integer")
+        if self.__width <= 0:
+            raise ValueError("width must be > 0")
 
     @property
     def height(self):
@@ -33,6 +74,12 @@ class Rectangle(Base):
     def height(self, value):
         """set height"""
         self.__height = value
+        try:
+            assert isinstance(value, int)
+        except:
+            raise TypeError("height must be an integer")
+        if self.__height <= 0:
+            raise ValueError("height must be > 0")
 
     @property
     def x(self):
@@ -42,7 +89,13 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """set x"""
-        self.__width = value
+        self.__x = value
+        try:
+            assert isinstance(self.__x, int)
+        except:
+            raise TypeError("x must be an integer")
+        if self.__x < 0:
+            raise ValueError("x must be >= 0")
 
     @property
     def y(self):
@@ -52,4 +105,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """set y"""
-        self.__y = y
+        self.__y = value
+        try:
+            assert isinstance(self.__y, int)
+        except BaseException:
+            raise TypeError("y must be an integer")
+        if self.__y < 0:
+            raise ValueError("y must be >= 0")
