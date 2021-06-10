366abb89d35fd11a13650bc82aaca9f8a807e2bd
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-10 17:32:07 +0300

    writes json representation of object to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,9 +15,21 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """return JSON string representation of list_dictionaries"""
-        if not list_dictionaries:
+        if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes json representation to a file"""
+        file_name = cls.__name__ + ".json"
+        new_list = []
+        if list_objs is not None:
+            for i in list_objs:
+                new_list.append(cls.to_dictionary(i))
+        with open(file_name, 'w') as json_file:
+            json_file.write(cls.to_json_string(new_list))
