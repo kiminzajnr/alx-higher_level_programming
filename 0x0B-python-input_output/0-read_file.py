08767314b8c401ab9f115049b13fbace2dd4a42e
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-20 11:43:25 +0300

    add loop to loop over lines

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
