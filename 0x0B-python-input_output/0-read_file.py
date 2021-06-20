8616547de62f7b47a36e03d6a94e38efd5dfdfbd
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-20 11:35:20 +0300

    reads a textfile and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module to read a text file"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
