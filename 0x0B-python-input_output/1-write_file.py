71ba35af2051e1d5b2e8c5ca16ed581be70bd876
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-20 12:06:07 +0300

    added documentation for function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file and returns
+    the number of characters written"""
     with open(filename, encoding="utf-8", mode="w+") as f:
         return f.write(text)
