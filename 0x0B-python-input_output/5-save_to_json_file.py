559db562dbb0bcc79e82f848b526ef74fde32741
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-20 13:31:45 +0300

    added dump to write to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a json representation"""
-    json_data = json.dumps(my_obj)
     with open(filename, mode="w") as f:
-        f.write(json_data)
+        json.dump(my_obj, f)
