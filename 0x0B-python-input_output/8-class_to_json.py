c523af50fbabcb9a41a1ac40cb01d10225b3615f
Erick Kiminza <kiminzaeric@gmail.com>
2021-06-20 17:21:53 +0300

    returns dictionary discription with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""returns dictionary description with sample data structure"""
+
+
+def class_to_json(obj):
+    """returns dictionary decription with sample data structure"""
+    return obj.__dict__
