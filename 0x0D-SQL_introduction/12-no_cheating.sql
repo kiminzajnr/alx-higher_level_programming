7c266fc9e27a18b371b7339df7047c8b2e0a7eb9
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-12 22:41:35 +0300

    updates the score of Bob to 10

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates the score of Bob to 10 in the table second_table
+-- database name will be passed as an argument of the mysql command
+UPDATE second_table SET score = 10 WHERE name = "Bob";
