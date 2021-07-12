b7b7e56283d1c8851904ce352110ee54328887b5
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-12 22:31:28 +0300

    lists all records of the table second_table

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table
+-- display both score and name, and database is passed as an argument of the mysql command
+SELECT score, name FROM second_table ORDER BY score DESC;
