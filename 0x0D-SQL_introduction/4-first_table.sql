a65457adb3197ba57c81118667fb38ec936808a9
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-12 21:32:05 +0300

    creates a table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates a table called firt_table in the current database
+-- database will be passed as argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table (id INT,
+		name VARCHAR(256)
+		);
