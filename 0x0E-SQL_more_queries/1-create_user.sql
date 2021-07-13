f9b3b3713ade041563642ec836d3b50834be69b2
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-13 17:11:47 +0300

    create user and grant all privilleges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates the mysql useruser_0d_1
+-- give the user all privileges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
