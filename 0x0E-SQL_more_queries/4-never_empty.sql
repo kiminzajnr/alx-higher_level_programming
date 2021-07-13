0550dfbaf84e93fc8d906259d884eb85d550a3de
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-13 17:58:34 +0300

    creates table id_not_null on mysql server

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- creates the table id_not_null on mysql server
+-- database name will be passed as an argument of mysql command
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1,
+		name VARCHAR(256)
+		);
