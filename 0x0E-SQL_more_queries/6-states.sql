f0d0e42506466b0fbee688a98ffa3e954dcfaa95
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-13 19:08:52 +0300

    create database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates database hbtn_0d_usa and table states
+-- if table or database exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
+		name VARCHAR(256) NOT NULL
+		);
