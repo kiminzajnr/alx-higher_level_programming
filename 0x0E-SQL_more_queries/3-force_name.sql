a119d4853e0fe9b9da31a6163ab2db928535f842
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-13 17:47:40 +0300

    creates table force_name on mysql server

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- creates the table force_name on mysql server
+-- database name passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS force_name (id INT,
+		name VARCHAR(256) NOT NULL
+		);
