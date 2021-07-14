ee8e2c2de0134edac2cea70cf37b8cae724e22ff
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-14 18:27:22 +0300

    add right join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- database name will be passed as an argument of the mysql command
-SELECT cities.id, cities.name, states.name FROM cities NATURAL JOIN states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities RIGHT JOIN states ON cities.id = states.id ORDER BY cities.id ASC;
