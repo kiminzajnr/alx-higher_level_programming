ecfb05ae2a852dda431645918d4107a3344464db
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-02 16:22:56 +0300

    fix output error

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const myArgs = process.argv.slice(2);
 if (isNaN(parseInt(myArgs[0]))) {
   console.log('Not a number');
 } else {
-  console.log(myArgs[0]);
+  console.log('My number:', parseInt(myArgs[0]));
 }
