a6c8ef4e6af7bb29e482feb4112dabfc0da74225
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-02 15:22:29 +0300

    fix output for no args passed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-console.log(myArgs[0], 'is', myArgs[1]);
+if (!myArgs[0]) {
+    console.log("undefined is undefined");
+}
+else {
+    console.log(myArgs[0], 'is', myArgs[1]);
+}
