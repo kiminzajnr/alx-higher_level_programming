afdf2026a07b07663ba564ca0817e567176d96ee
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-02 16:36:14 +0300

    print contents of an array

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const arrayLength = myLines.length;
+let i = 0;
+while (i < arrayLength) {
+  console.log(myLines[i]);
+  i++;
+}
