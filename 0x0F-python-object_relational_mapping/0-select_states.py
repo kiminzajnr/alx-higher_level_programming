9a621c00c1dde18113e7af399d190a4378bced72
kiminzajnr <kiminzaeric@gmail.com>
2021-08-07 05:49:27 +0000

    fix pep8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,13 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3], charset = 'utf8')
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset='utf8')
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
