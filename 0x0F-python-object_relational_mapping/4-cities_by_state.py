718d769b78e0922b472e41800132663097856ee2
kiminzajnr <kiminzaeric@gmail.com>
2021-08-07 07:51:55 +0000

    JOIN states ON cities.state_id = states.id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute(
         "SELECT cities.id, cities.name, states.name FROM cities JOIN \
-states ON cities.id = states.id ORDER BY cities.id ASC")
+states ON cities.state_id = states.id ORDER BY cities.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
