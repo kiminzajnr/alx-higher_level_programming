b76d491cb767bb98539ccd62c42dbc514b15ba69
kiminzajnr <kiminzaeric@gmail.com>
2021-08-07 07:06:31 +0000

    fix pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
+"""takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections
+"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1], passwd = sys.argv[2], db = sys.argv[3], charset = "utf8")
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name = %s ORDER BY id ASC""", (sys.argv[4],))
+    cur.execute(
+        """SELECT * FROM states WHERE \
+name = %s ORDER BY id ASC""", (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
