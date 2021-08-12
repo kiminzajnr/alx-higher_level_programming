efb7b779a6763e19f50f4ae1b754689886ebab73
kiminzajnr <kiminzaeric@gmail.com>
2021-08-12 13:39:58 +0000

    add advanced task

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""contains class difinition of city
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """inherits from Base"""
+    __tablename__ = "cities"
+    id = Column(
+        Integer,
+        nullable=False,
+        primary_key=True,
+        unique=True,
+        autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
