75c83c0849340220beab099387f622ab790d491c
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-18 16:23:08 +0300

    add  to dipict columns

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- lists all cities contained in the database hbtn_0d_usa
 -- database name will be passed as an argument of the mysql command
-SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.id = states.id ORDER BY cities.id;
+SELECT c.`id`, c.`name`, s.`name` FROM `cities` AS c INNER JOIN `states` AS s ON c.`state_id` = s.`id` ORDER BY c.`id`;
