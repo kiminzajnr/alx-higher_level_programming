8e4fa2e4be271b8125afd99768dfa7dc350f4d1d
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-18 18:28:55 +0300

    use left join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Lists all shows contained in database hbtn_0d_tvshows
+-- if show doesn't have a genre, display NULL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;
