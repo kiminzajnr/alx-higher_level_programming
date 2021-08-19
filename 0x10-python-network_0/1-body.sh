a74c58f09145bcfd86b34ceb0cd6b24cbf9f8c3a
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-19 20:13:38 +0300

    fix to display only body of a 200 status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL and displays the body of the response
-curl -sb /dev/null -X GET "$1"
+status=$(curl -o /dev/null -s -w '%{http_code}' "$1"); [[ $status == 200 ]] &&  curl -sb -X GET "$1"
