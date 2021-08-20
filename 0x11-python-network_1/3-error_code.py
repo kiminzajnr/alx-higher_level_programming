08ef19c918cc2b819efff98f91f0bc504d789062
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-20 13:09:27 +0300

    fix incorrect output

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,13 +4,12 @@ and displays the body of the response
 """
 if __name__ == "__main__":
     import sys
-    from urllib.error import HTTPError
-    from urllib.request import Request, urlopen
-    request = Request(sys.argv[1])
+    import urllib.request
+    request = urllib.request.Request(sys.argv[1])
     try:
-        response = urlopen(request)
-    except HTTPError as e:
-        print("Error code: ", e.code)
+        response = urllib.request.urlopen(request)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
     else:
         the_page = response.read().decode('utf-8')
         print(the_page)
