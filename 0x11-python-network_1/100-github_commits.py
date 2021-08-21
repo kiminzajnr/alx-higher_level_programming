effd630c55ff55ffdd30d77bd73ccce1a5ec5526
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-21 11:33:16 +0300

    Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of the
+repository "rails" by user "rails" you must use the githubapi
+Print all commits by: <sha>: <author name>` (one by line)
+"""
+if __name__ == "__main__":
+    import sys
+    import requests
+    repo = sys.argv[1] + "/" + sys.argv[2]
+    url = 'https://api.github.com/repos/{0}/commits?per_page=10'.format(repo)
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits:
+        sha = commit.get('sha')
+        name = commit.get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
