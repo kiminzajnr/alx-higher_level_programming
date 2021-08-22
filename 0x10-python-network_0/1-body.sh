ed535728e1e10dc953418dcb3f64c0e55583fdc8
Erick Kiminza <kiminzaeric@gmail.com>
2021-08-22 13:11:43 +0300

    add -L for redirect

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL and displays the body of the response
-status=$(curl -o /dev/null -s -w '%{http_code}' "$1"); [[ $status == 200 ]] &&  curl -sb -X GET "$1"
+curl -sL "$1"
