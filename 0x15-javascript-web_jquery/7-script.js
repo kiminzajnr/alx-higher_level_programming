2e1f89b7df76c6fd940ae68c711e1275f216fe23
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-22 20:10:21 +0300

    add task 7. Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+  * fetches the character name from https://swapi-api.hbtn.io/api/people/5/?format=json
+  * you must use JQuery API
+  */
+const $ = window.$;
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+$.get(url, function (data) {
+  const name = data.name;
+  $('DIV#character').text(name);
+  console.log(data);
+});
