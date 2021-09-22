b2186d9c9f9945ac3e0102395763df68030c4343
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-22 20:14:52 +0300

    remove console.log

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -8,5 +8,4 @@ const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 $.get(url, function (data) {
   const name = data.name;
   $('DIV#character').text(name);
-  console.log(data);
 });
