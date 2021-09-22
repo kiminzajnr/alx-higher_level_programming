b322c4f48538161a6da127f08aa5a63bd905b79f
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-22 16:38:49 +0300

    add task 0. No JQuery:

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,7 @@
+/**
+  * updates the color of the <header> element to red
+  */
+window.onload = function myFunction () {
+  const elem = document.querySelector('header');
+  elem.style.color = 'red';
+};
