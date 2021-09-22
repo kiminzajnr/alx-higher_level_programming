85e8999cc1da09068bf58c000b68cb4773f219e2
Erick Kiminza <kiminzaeric@gmail.com>
2021-09-22 19:36:31 +0300

    add task 5. List of elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/**
+  * adds a <li> element to a list when the user clicks on the tag DIV#add_item
+  * use JQuery
+  */
+const $ = window.$;
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+});
