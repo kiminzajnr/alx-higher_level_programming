f5297b9ca332c518729ab9d22d22f36c769a81aa
kiminzajnr <kiminzaeric@gmail.com>
2022-03-15 18:05:13 +0300

    prints all names defined by the compiled file hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from hidden_4 import *
+import hidden_4
 if __name__ == "__main__":
-    for name in names:
-        print(name)
+    for name in dir(hidden_4):
+        if "__" not in name:
+            print(name)
