2369437d09b3104dc1518227e6196e616f0fac8f
kiminzajnr <kiminzaeric@gmail.com>
2022-03-16 19:55:05 +0300

    fix output error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
     """ this function prints a matrix of integers"""
     for row in matrix:
         for elem in row:
-            print(" {:d}".format(elem), end="")
-        print()
+            print("{:d}".format(elem), end=" "
+                  if elem != row[-1] else '\n')
