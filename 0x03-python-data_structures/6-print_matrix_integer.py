76ade88702dc4bb68c5a1834e84bbfa514ac31cd
kiminzajnr <kiminzaeric@gmail.com>
2022-03-16 19:57:44 +0300

    for empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     """ this function prints a matrix of integers"""
-    for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end=" "
-                  if elem != row[-1] else '\n')
+    if matrix != [[]]:
+        for row in matrix:
+            for elem in row:
+                print("{:d}".format(elem), end=" "
+                      if elem != row[-1] else '\n')
+    else:
+        print()
