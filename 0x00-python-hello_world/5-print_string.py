5eb33bf08cf563d4039d07c8f2193d3e325dbbbe
kiminzajnr <kiminzaeric@gmail.com>
2022-08-24 20:54:46 +0300

    test

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print("{}\n{}".format(str * 3, str[:9]))
+print(f"{str * 3}")
+print(f"{str[ :9]}")
---
+++ 0x00-python-hello_world/hello.py
@@ -0,0 +1,3 @@
+def hello():
+	print("I'm here")
+hello()
