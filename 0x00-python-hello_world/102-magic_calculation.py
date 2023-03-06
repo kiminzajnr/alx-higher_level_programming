b49219a56741dab32ecf7bdca7a73dcce111a040
kiminzajnr <kiminzaeric@gmail.com>
2023-03-06 16:40:49 +0300

    script for bytecode

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """calculation"""
-import dis
+
 
 def magic_calculation(a, b):
     """does exactly same as bytecode given"""
     const = 98
     result = pow(a, b) + const
     return result
-print(dis.dis(magic_calculation))
