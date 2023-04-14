f42fb609a493615856dfe9cf1ed4124610be6f46
kiminzajnr <kiminzaeric@gmail.com>
2023-04-14 22:38:31 +0300

    remove reduntant code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,14 @@ class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
         """Instantiation of width and height"""
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        # if not isinstance(width, int):
+        #     raise TypeError("width must be an integer")
+        # if width < 0:
+        #     raise ValueError("width must be >= 0")
+        # if not isinstance(height, int):
+        #     raise TypeError("height must be an integer")
+        # if height < 0:
+        #     raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
