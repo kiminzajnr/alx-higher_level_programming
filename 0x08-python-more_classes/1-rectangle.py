b331552e17c2bc972ff8e4de84f8db7052dd3347
kiminzajnr <kiminzaeric@gmail.com>
2023-04-14 22:50:17 +0300

    remove duplicate code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,14 @@ class Rectangle:
     """rectangle class"""
     def __init__(self, width=0, height=0):
         """Instantiation of width and height"""
-        # if not isinstance(width, int):
-        #     raise TypeError("width must be an integer")
-        # if width < 0:
-        #     raise ValueError("width must be >= 0")
-        # if not isinstance(height, int):
-        #     raise TypeError("height must be an integer")
-        # if height < 0:
-        #     raise ValueError("height must be >= 0")
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
@@ -32,27 +32,17 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set height"""
-        # if not isinstance(value, int):
-        #     raise TypeError("width must be an integer")
-        # if value < 0:
-        #     raise ValueError("width must be >= 0")
-        self.__width = value
-        try:
-            assert type(self.__width) == int
-        except BaseException:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @height.setter
     def height(self, value):
         """Set height"""
-        # if not isinstance(value, int):
-        #     raise TypeError("height must be an integer")
-        # if value < 0:
-        #     raise ValueError("height must be >= 0")
-        self.__height = value
-        try:
-            assert type(self.__height) == int
-        except BaseException:
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
